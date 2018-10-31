68822fa112034978cb90cebf392976025033acc2
bigcircle448 <yoshiya32@hotmail.com>
2018-10-31 17:34:34 +0900

    2018/10/31

---
+++ nandemoii/src/nandemoii/Classmei.java
@@ -0,0 +1,15 @@
+package nandemoii;
+
+public class Classmei {
+
+	public static void main(String[] args) {
+	System.out.println("Hello Akki");
+	System.out.println(tekitou());
+	}
+	public static String tekitou(){
+		return "風見";
+	}
+
+
+
+}
